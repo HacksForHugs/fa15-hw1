e77a245b8f014b9d3854435d799ce41acbd8f8c2
Helen Che <helenc@berkeley.edu>
2015-09-18 13:17:57 -0700

    finished q4

--- foobar.rb
+++ foobar.rb
@@ -1,12 +1,13 @@
 class Foobar
   # Q4 CODE HERE
-  def baz(a)
+  
+  def self.baz(a) 
   	a.uniq!
-  	a.map{|x| x.to_int + 2}
-  	a.keep{|x| x < 10}
+   	a.map!{|x| x.to_i + 2}
+  	a.select!{|x| x%2 == 0 && x < 10}
   	temp=0
   	for x in a
-  		temp +=x
+  		temp += x
   	end
   	return temp
 
